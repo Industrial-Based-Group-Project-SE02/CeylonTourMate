8a92b09956ed13d47a328734b2930d6e43b32fac
oshi2002 <oshani.kaveesha2002@gmail.com>
2025-12-12 23:55:09 +0530

    first commit

---
+++ my-fullstack-app/client/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Users from './pages/Users';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <div className="relative">
+          <div className="absolute top-0 right-0 left-0 z-50">
+            <Navbar />
+          </div>
+          
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/users" element={<Users />} />
+          </Routes>
+        </div>
+        
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
